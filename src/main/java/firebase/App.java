fef83b5fae49a05f190dbea70203038ab3abb3e4
sweetpotatoju <jeju3146@naver.com>
2022-07-27 21:54:36 +0900

    dependency add

---
+++ src/main/java/firebase/App.java
@@ -0,0 +1,32 @@
+package firebase;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+
+    public class App {
+
+        public void initFirebase(){
+
+            try {
+                FileInputStream serviceAccount = new FileInputStream("moon.json");
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://moonlander1-c4ddb-default-rtdb.firebaseio.com/")
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            } catch (IOException e) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+
+    }
+
---
+++ src/main/java/moon_lander/login.java
@@ -0,0 +1,120 @@
+package moon_lander;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class login {
+
+
+    public class LoginPage extends JFrame {
+        //id 입력 필드
+        private JTextField tID;
+        //클리어 버튼
+        private JButton btnClear;
+        //ok 버튼
+        private JButton btnOK;
+        //로그인 패널
+        private JPanel loginPanel;
+        //pw 입력 필드
+        private JPasswordField tpw;
+        //회원가입 버튼
+        private JButton btnResister;
+        //메세지 출력 라벨
+        private JLabel message;
+        //사용자 아이디
+        private static String ID = null;
+        //사용자 비밀번호
+        private char[] pw = null;
+
+
+        public LoginPage() {
+            setContentPane(loginPanel);
+            setTitle("Login");
+            setSize(450, 300);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setVisible(true);
+
+            Dimension frameSize = getSize();
+            Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+            //화면을 중앙에 띄우기
+            setLocation((windowSize.width - frameSize.width) / 2, (windowSize.height - frameSize.height) / 2);
+
+            //OK버튼
+            btnOK.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ID = tID.getText();
+                    pw = tpw.getPassword();
+
+                    if (ID.isEmpty() || Arrays.toString(pw).isEmpty()){
+                        message.setText("아이디 또는 비밀번호를 입력하세요");
+                    }
+                    else {
+                        getDataByEmail();
+                    }
+                }
+            });
+
+            //clear 버튼
+            btnClear.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    tID.setText("");
+                    tpw.setText("");
+                }
+            });
+
+            //resister 버튼
+            btnResister.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    new resister();
+                   dispose();
+                }
+            });
+        }
+
+        private void getDataByEmail(){
+
+            UserRecord userRecord = null;
+            try {
+                userRecord = FirebaseAuth.getInstance().getUserByEmail(tID.getText());
+                String email = ((UserRecord) userRecord).getEmail();
+                String uid = userRecord.getUid();
+                String password = userRecord.getDisplayName();
+
+                if (password.equals(String.valueOf(tpw.getPassword()))){
+                    JOptionPane.showMessageDialog(null, "Hello" + " " + email);
+                    setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(null, "비밀번호가 일치하지 않습니다.");
+                }
+
+                recoverUserData(uid);
+            } catch (FirebaseAuthException ex) {
+                Logger.getLogger(resister.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        private void recoverUserData(String uid){
+            UserRecord userRecord = null;
+            try {
+                userRecord = FirebaseAuth.getInstance().getUser(uid);
+
+                Logger.getLogger(LoginPage.class.getName()).log(Level.INFO, "유저 데이터를 성공적으로 Fetch:");
+                Logger.getLogger(LoginPage.class.getName()).log(Level.INFO, userRecord.getUid());
+            } catch (FirebaseAuthException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
